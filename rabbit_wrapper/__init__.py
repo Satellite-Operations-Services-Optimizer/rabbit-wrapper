e611909a2af7937d1df40e141c30e872658a46c1
stan15 <stanihe1901@gmail.com>
2023-11-08 20:53:44 -0500

    changed __main__.py to __init__.py to export correct stuff

---
+++ rabbit_wrapper/__init__.py
@@ -0,0 +1,3 @@
+from .rabbit import Rabbit
+from .direct.consumer import Consumer
+from .direct.publisher import Publisher
--- rabbit_wrapper/__main__.py
+++
@@ -1,3 +0,0 @@
-from rabbit import Rabbit
-from direct.consumer import Consumer
-from direct.publisher import Publisher
