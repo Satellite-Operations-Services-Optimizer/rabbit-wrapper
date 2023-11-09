cae06e201eed057bbc66d2a47b9f4289d97ee375
stan15 <stanihe1901@gmail.com>
2023-11-08 21:06:03 -0500

    minor change

--- rabbit_wrapper/rabbit.py
+++ rabbit_wrapper/rabbit.py
@@ -8,13 +8,13 @@ class Rabbit:
             self.connection = BlockingConnection(user, password, host, port, vhost)
         else:
             self.connection = SelectConnection(user, password, host, port, vhost)
-        self.connection.connect()
+        self.connect()
         self.channel = self.connection.channel
 
     def declare_queue(
         self, queue_name, exclusive: bool = False
     ):
-        self.connection.check_connection()
+        self.check_connection()
         logger.debug(f"Trying to declare queue({queue_name})...")
         self.channel.queue_declare(
             queue=queue_name,
@@ -23,13 +23,13 @@ class Rabbit:
         )
 
     def declare_exchange(self, exchange_name: str, exchange_type: str = "direct"):
-        self.connection.checn_connection()
+        self.check_connection()
         self.channel.exchange_declare(
             exchange=exchange_name, exchange_type=exchange_type
         )
 
     def bind_queue(self, exchange_name: str, queue_name: str, routing_key: str):
-        self.connection.check_connection()
+        self.check_connection()
         self.channel.queue_bind(
             exchange=exchange_name, queue=queue_name, routing_key=routing_key
         )
@@ -40,8 +40,7 @@ class Rabbit:
         )
     
     def check_connection(self):
-        self.connection.check_connection()
+        return self.connection.check_connection()
 
     def close(self):
         self.connection.close()
-
